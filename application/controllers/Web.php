328d974c6161c4fd61698f724ac37365d4b383eb
nxrmalaa <malaa200300@gmail.com>
2022-04-03 13:27:01 +0700

    merevisi error pada application/controllers/Web.php application/views/v_footer.php application/views/v_header.php application/view

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -1,5 +1,5 @@
 <?php 
-defined(‘BASEPATH’) or exit (‘no direct script access allowed’); 
+defined('BASEPATH') or exit ('no direct script access allowed'); 
 
 class Web extends CI_Controller
 { 
@@ -9,9 +9,9 @@ class Web extends CI_Controller
     } 
     public function index()
     { 
-        $data[‘judul’] = “Halaman Depan”; 
-        $this->load->view(‘v_header’,$data); 
-        $this->load->view(‘v_index’,$data);
-        $this->load->view(‘v_footer’,$data); 
+        $data['judul'] = "Halaman Depan"; 
+        $this->load->view('v_header',$data); 
+        $this->load->view('v_index',$data);
+        $this->load->view('v_footer',$data); 
     } 
 }
--- application/views/v_index.php
+++ application/views/v_index.php
@@ -2,7 +2,7 @@
     <h1>
         <?php echo $judul ?>
     </h1> 
-    <p align=’justify’>Pada pengertian codeigniter di atas 
+    <p align='justify'>Pada pengertian codeigniter di atas 
         tadi di jelaskan bahwa codeigniter menggunakan metode MVC. Apa itu 
         MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih 
         jauh dalam belajar codeigniter.
@@ -12,7 +12,7 @@
     </p> 
     <ol type=”a”> 
         <li>Model</li> 
-        <p align=’justify’>Model adalah kelas yang merepresentasikan atau 
+        <p align='justify'>Model adalah kelas yang merepresentasikan atau 
             memodelkan tipe data yang akan digunakan oleh aplikasi. Model 
             juga dapat didefinisakn sebagai bagian penanganan yang berhubungan 
             dengan pengolahan atau manipulasi database. Seperti misalnya mengambil 
@@ -22,24 +22,24 @@
             untuk menghitung luas dan keliling lingkaran, maka dapat memodelkan 
             objek lingkaran sebagai kelas model.
         </p> 
-        <p align=’justify’>Sebagai catatan, Semua model harus disimpan di dalam 
+        <p align='justify'>Sebagai catatan, Semua model harus disimpan di dalam 
             folder application\models
         </p> 
         <li>View</li> 
-        <p align=’justify’>View merupakan bagian yang menangani halaman user 
+        <p align='justify'>View merupakan bagian yang menangani halaman user 
             interface atau halaman yang muncul pada user(pada browser). Tampilan 
             dari user interface di kumpulkan pada view untuk memisahkannya dengan 
             controller dan model sehingga memudahkan web designer dalam melakukan 
             pengembangan tampilan halaman website.
         </p> 
         <li>Controller</li> 
-        <p align=’justify’>Controller merupakan kumpulan intruksi aksi yang 
+        <p align='justify'>Controller merupakan kumpulan intruksi aksi yang 
             menghubungkan model dan view, jadi user tidak akan berhubungan dengan 
             model secara langsung, intinya data yang tersimpan di database (model) 
             di ambil oleh controller dan kemudian controller pula yang menampilkan 
             nya ke view. Jadi controller lah yang mengolah intruksi.
         </p> 
-        <p align=’justify’>Dari penjelasan tentang model view dan controller di atas 
+        <p align='justify'>Dari penjelasan tentang model view dan controller di atas 
             dapat di simpulkan bahwa controller sebagai penghubung view dan model. 
             Misalnya pada aplikasi yang menampilkan data dengan menggunakan metode 
             konsep mvc, controller memanggil intruksi pada model yang mengambil data 
@@ -52,3 +52,4 @@
             dapat di lakukan dengan cepat dan terstruktur.
         </p> 
     </section>
+    
