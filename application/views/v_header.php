3d715810154ec117dca21a02dd1dca472f763e8a
nxrmalaa <malaa200300@gmail.com>
2022-04-03 13:44:51 +0700

    merevisi base url pada application/views/v_header.php

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -3,7 +3,7 @@
     <head> 
         <meta charset="utf-8"> 
         <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title> 
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css"> 
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/stylebuku.css"> 
     </head> 
     <body> 
         <div id="wrapper"> 
